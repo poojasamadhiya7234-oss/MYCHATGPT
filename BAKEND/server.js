559b7c6f35c1184c575d9bdcddaac4f60b3d28e2
poojasamadhiya7234-oss <poojasamadhiya7234@gmail.com>
2026-02-15 13:18:00 +0530

    fix db connection

--- BAKEND/server.js
+++ BAKEND/server.js
@@ -1,10 +1,48 @@
+// import express from "express";
+// import cors from "cors";
+// import dotenv from "dotenv";
+// import Groq from "groq-sdk";
+// import mongoose from "mongoose";
+
+// import chatRoutes from "./routes/chat.js"
+
+// dotenv.config();
+
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
+
+// app.use("/api",chatRoutes)
+
+
+// // Server run
+// const PORT = process.env.PORT || 5000;
+
+// app.listen(PORT, () => {
+//   console.log("Server running on port", PORT);
+// });
+
+
+
+// const connectDB=async()=>{
+//   try{
+//     await mongoose.connect(process.env.MONGODB_URI);
+//     console.log("Connected with Database");
+//   }
+//   catch(err){
+//     console.log("Failed to connect with Db",err);
+
+//   }
+
+// }
+
+
+
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import Groq from "groq-sdk";
 import mongoose from "mongoose";
-
-import chatRoutes from "./routes/chat.js"
+import chatRoutes from "./routes/chat.js";
 
 dotenv.config();
 
@@ -12,29 +50,32 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api",chatRoutes)
+app.use("/api", chatRoutes);
 
+// Home route
+app.get("/", (req, res) => {
+  res.send("Backend is running...");
+});
+
+// DB Connect
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("✅ Connected with Database");
+  } catch (err) {
+    console.log("❌ Failed to connect with DB", err);
+  }
+};
+
+// CALL DB CONNECT
+connectDB();
 
 // Server run
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => {
-  console.log("Server running on port", PORT);
+  console.log("✅ Server running on port", PORT);
 });
 
-
-
-const connectDB=async()=>{
-  try{
-    await mongoose.connect(process.env.MONGODB_URI);
-    console.log("Connected with Database");
-  }
-  catch(err){
-    console.log("Failed to connect with Db",err);
-
-  }
-
-}
 
 
 
